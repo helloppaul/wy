b7d95b176d8f5d55d265ea95152bd148d0f1d44f
unknown <huajk@chinacscs.com>
2022-11-23 18:53:08 +0800

    2022-11-23 预警分配置表 合预警等级等级划分档位-配置表 更新

--- 上线脚本/预警分系(未上线)/预警分-配置表/预警分-配置表.sql
+++ 上线脚本/预警分系(未上线)/预警分-配置表/预警分-配置表.sql
@@ -5,39 +5,39 @@ create table pth_rmp.rmp_warn_level_ratio_cfg as
 with
 warn_level_ratio_cfg_ as -- 综合预警等级等级划分档位-配置表
 (
-	select 2 property_cd,'城投' as property,'-5' as warn_lv,'前1%' as percent_desc,'风险已暴露' as warn_lv_desc
+	select 2 property_cd,'城投' as property,'-5' as warn_lv,'3%' as percent_desc,'风险已暴露' as warn_lv_desc
 	union all
-	select 2 property_cd,'城投' as property,'-4' as warn_lv,'前1%-10%' as percent_desc,'红色预警等级' as warn_lv_desc
+	select 2 property_cd,'城投' as property,'-4' as warn_lv,'3%-15%' as percent_desc,'红色预警等级' as warn_lv_desc
 	union all
-	select 2 property_cd,'城投' as property,'-3' as warn_lv,'前10%-20%' as percent_desc,'橙色预警等级' as warn_lv_desc
+	select 2 property_cd,'城投' as property,'-3' as warn_lv,'15%-26%' as percent_desc,'橙色预警等级' as warn_lv_desc
 	union all
-	select 2 property_cd,'城投' as property,'-2' as warn_lv,'前20%-30%' as percent_desc,'黄色预警等级' as warn_lv_desc
+	select 2 property_cd,'城投' as property,'-2' as warn_lv,'26%-42%' as percent_desc,'黄色预警等级' as warn_lv_desc
 	union all
-	select 2 property_cd,'城投' as property,'-1' as warn_lv,'后30%-100%' as percent_desc,'绿色预警等级' as warn_lv_desc 
+	select 2 property_cd,'城投' as property,'-1' as warn_lv,'42%-100%' as percent_desc,'绿色预警等级' as warn_lv_desc 
 	
 	union all
 
-	select 1 property_cd,'产业' as property,'-5' as warn_lv,'前1%' as percent_desc,'风险已暴露' as warn_lv_desc
+	select 1 property_cd,'产业' as property,'-5' as warn_lv,'3%' as percent_desc,'风险已暴露' as warn_lv_desc
 	union all
-	select 1 property_cd,'产业' as property,'-4' as warn_lv,'前1%-10%' as percent_desc,'红色预警等级' as warn_lv_desc
+	select 1 property_cd,'产业' as property,'-4' as warn_lv,'3%-12%' as percent_desc,'红色预警等级' as warn_lv_desc
 	union all
-	select 1 property_cd,'产业' as property,'-3' as warn_lv,'前10%-20%' as percent_desc,'橙色预警等级' as warn_lv_desc
+	select 1 property_cd,'产业' as property,'-3' as warn_lv,'12%-20%' as percent_desc,'橙色预警等级' as warn_lv_desc
 	union all
-	select 1 property_cd,'产业' as property,'-2' as warn_lv,'前20%-30%' as percent_desc,'黄色预警等级' as warn_lv_desc
+	select 1 property_cd,'产业' as property,'-2' as warn_lv,'20%-36%' as percent_desc,'黄色预警等级' as warn_lv_desc
 	union all
-	select 1 property_cd,'产业' as property,'-1' as warn_lv,'后30%-100%' as percent_desc,'绿色预警等级' as warn_lv_desc 
+	select 1 property_cd,'产业' as property,'-1' as warn_lv,'36%-100%' as percent_desc,'绿色预警等级' as warn_lv_desc 
 
 	union all
 
-	select 0 property_cd,'产业' as property,'-5' as warn_lv,'前1%' as percent_desc,'风险已暴露' as warn_lv_desc
+	select 0 property_cd,'产业' as property,'-5' as warn_lv,'3%' as percent_desc,'风险已暴露' as warn_lv_desc
 	union all
-	select 0 property_cd,'产业' as property,'-4' as warn_lv,'前1%-10%' as percent_desc,'红色预警等级' as warn_lv_desc
+	select 0 property_cd,'产业' as property,'-4' as warn_lv,'3%-12%' as percent_desc,'红色预警等级' as warn_lv_desc
 	union all
-	select 0 property_cd,'产业' as property,'-3' as warn_lv,'前10%-20%' as percent_desc,'橙色预警等级' as warn_lv_desc
+	select 0 property_cd,'产业' as property,'-3' as warn_lv,'12%-20%' as percent_desc,'橙色预警等级' as warn_lv_desc
 	union all
-	select 0 property_cd,'产业' as property,'-2' as warn_lv,'前20%-30%' as percent_desc,'黄色预警等级' as warn_lv_desc
+	select 0 property_cd,'产业' as property,'-2' as warn_lv,'20%-36%' as percent_desc,'黄色预警等级' as warn_lv_desc
 	union all
-	select 0 property_cd,'产业' as property,'-1' as warn_lv,'后30%-100%' as percent_desc,'绿色预警等级' as warn_lv_desc 
+	select 0 property_cd,'产业' as property,'-1' as warn_lv,'36%-100%' as percent_desc,'绿色预警等级' as warn_lv_desc 
 )
 select * from warn_level_ratio_cfg_
 ;
