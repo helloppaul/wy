1563694d1a9ca41f0bcb8e32907bab769f9ac847
unknown <huajk@chinacscs.com>
2022-11-24 11:00:33 +0800

    2022-11-24 预警分模型结果表-初始化 更新

--- 数据初始化/预警分系/预警分模型结果/rmp_warning_score_model_init.sql
+++ 数据初始化/预警分系/预警分模型结果/rmp_warning_score_model_init.sql
@@ -26,7 +26,7 @@ stored as textfile;
 
 --（2）sql初始化 impala执行 --
 drop table if exists pth_rmp.rmp_warning_score_model_init_impala;
-create table pth_rmp.rmp_warning_score_model_init_impala as 
+create table pth_rmp.rmp_warning_score_model_init_impala stored as parquet as 
 --—————————————————————————————————————————————————————— 基本信息 ————————————————————————————————————————————————————————————————————————————————--
 with
 corp_chg as  --带有 城投/产业判断和国标一级行业 的特殊corp_chg
@@ -42,17 +42,62 @@ corp_chg as  --带有 城投/产业判断和国标一级行业 的特殊corp_chg
 		  	-- on b1.etl_date=b2.etl_date
 		) b 
 		on a.corp_id=b.corp_id --and a.etl_date = b.etl_date
-	where a.delete_flag=0 and b.delete_flag=0
+	where a.delete_flag=0 and b.delete_flag=0 and a.source_code='ZXZX'
 ),
 --—————————————————————————————————————————————————————— 接口层 ————————————————————————————————————————————————————————————————————————————————--
--- 预警分 --
-_rsk_rmp_warncntr_dftwrn_rslt_union_adj_intf_  as --预警分_融合调整后综合  原始接口
+-- 模型版本控制 --
+model_version_intf_ as   --@hds.t_ods_ais_me_rsk_rmp_warncntr_dftwrn_conf_modl_ver_intf   @app_ehzh.rsk_rmp_warncntr_dftwrn_conf_modl_ver_intf
 (
-    select * 
-    from hds.tr_ods_ais_me_rsk_rmp_warncntr_dftwrn_rslt_union_adj_intf  --@hds.tr_ods_ais_me_rsk_rmp_warncntr_dftwrn_rslt_union_adj_intf
-	where 1=1
-	  and to_date(rating_dt) >= to_date('2020-01-01') 
-	  and to_date(rating_dt) <= to_date('2022-10-30')
+    select 'creditrisk_lowfreq_concat' model_name,'v1.0.4' model_version,'active' status  --低频模型
+    union all
+    select 'creditrisk_midfreq_cityinv' model_name,'v1.0.4' model_version,'active' status  --中频-城投模型
+    union all 
+    select 'creditrisk_midfreq_general' model_name,'v1.0.2' model_version,'active' status  --中频-产业模型
+    union all 
+    select 'creditrisk_highfreq_scorecard' model_name,'v1.0.4' model_version,'active' status  --高频-评分卡模型(高频)
+    union all 
+    select 'creditrisk_highfreq_unsupervised' model_name,'v1.0.2' model_version,'active' status  --高频-无监督模型
+    union all 
+    select 'creditrisk_union' model_name,'v1.0.2' model_version,'active' status  --信用风险综合模型
+    -- select 
+    --     notes,
+    --     model_name,
+    --     model_version,
+    --     status,
+    --     etl_date
+    -- from hds.t_ods_ais_me_rsk_rmp_warncntr_dftwrn_conf_modl_ver_intf a
+    -- where a.etl_date in (select max(etl_date) from t_ods_ais_me_rsk_rmp_warncntr_dftwrn_conf_modl_ver_intf)
+    --   and status='active'
+    -- group by notes,model_name,model_version,status,etl_date
+),
+-- 时间限制开关 --
+timeLimit_switch as 
+(
+    select false as flag   --TRUE:时间约束，FLASE:时间不做约束，通常用于初始化
+    -- select False as flag
+),
+-- 预警分 --
+rsk_rmp_warncntr_dftwrn_rslt_union_adj_intf_  as --预警分_融合调整后综合  原始接口
+(
+	select a.*
+    from 
+    (
+		select m.*
+		from
+		(
+			-- 时间限制部分 --
+			select *,rank() over(partition by to_date(rating_dt) order by etl_date desc ) as rm
+			from hds.tr_ods_ais_me_rsk_rmp_warncntr_dftwrn_rslt_union_adj_intf  --@hds.tr_ods_ais_me_rsk_rmp_warncntr_dftwrn_rslt_union_adj_intf
+			where 1 in (select max(flag) from timeLimit_switch) 
+			and to_date(rating_dt) = to_date(from_unixtime(unix_timestamp(cast(${ETL_DATE} as string),'yyyyMMdd')))
+			union all
+			-- 非时间限制部分 --
+			select * ,rank() over(partition by to_date(rating_dt) order by etl_date desc ) as rm
+			from hds.tr_ods_ais_me_rsk_rmp_warncntr_dftwrn_rslt_union_adj_intf  --@hds.tr_ods_ais_me_rsk_rmp_warncntr_dftwrn_rslt_union_adj_intf
+			where 1 in (select not max(flag) from timeLimit_switch) 
+		) m where rm=1
+    ) a join model_version_intf_ b
+        on a.model_version = b.model_version and a.model_name=b.model_name
 ),
 --—————————————————————————————————————————————————————— 应用层 ————————————————————————————————————————————————————————————————————————————————--
 warn_union_adj_sync_score as --取最新批次的融合调整后综合预警分
@@ -85,14 +130,15 @@ warn_union_adj_sync_score as --取最新批次的融合调整后综合预警分
 		end as adjust_warnlevel,
 		a.model_name,
 		a.model_version
-    from _rsk_rmp_warncntr_dftwrn_rslt_union_adj_intf_ a   
-    join (select max(rating_dt) as max_rating_dt,to_date(rating_dt) as score_dt from _rsk_rmp_warncntr_dftwrn_rslt_union_adj_intf_ group by to_date(rating_dt)) b
+    from rsk_rmp_warncntr_dftwrn_rslt_union_adj_intf_ a   
+    join (select max(rating_dt) as max_rating_dt,to_date(rating_dt) as score_dt from rsk_rmp_warncntr_dftwrn_rslt_union_adj_intf_ group by to_date(rating_dt)) b
         on a.rating_dt=b.max_rating_dt and to_date(a.rating_dt)=b.score_dt
     join corp_chg chg
         on chg.source_code='ZXZX' and chg.source_id=cast(a.corp_code as string)
 )
-------------------------------------以上部分为临时表（生成的每天的数据都是基于每天中的一个批次）-------------------------------------------------------------------
-select distinct
+------------------------------------以上部分为临时表-------------------------------------------------------------------
+-- insert into pth_rmp.RMP_WARNING_SCORE_MODEL partition(etl_date=${ETL_DATE})
+select 
 	-- md5(concat(batch_dt,corp_id,cast(score_date as string),model_version)) as sid_kw,  --hive
 	batch_dt,
 	corp_id,
@@ -110,13 +156,14 @@ select distinct
 	current_timestamp() update_time,
 	0 as version
 from warn_union_adj_sync_score
-WHERE score_date>=to_date('2022-09-24') 
+-- WHERE score_date=to_date(date_add(from_unixtime(unix_timestamp(cast(${ETL_DATE} as string),'yyyyMMdd')),0))
 ;
 
---（3）sql初始化 RMP_WARNING_SCORE_MODEL_INIT impala执行 --
-insert into pth_rmp.rmp_warning_score_model_init partition(etl_date=19900101) 
+
+--（3） rmp_warning_score_model_init hve执行 --
+insert into pth_rmp.rmp_warning_score_model_init partition(etl_date=19900101)
 select 
-	md5(concat(batch_dt,corp_id,cast(score_date as string),model_version))  as sid_kw,
+	md5(concat(batch_dt,corp_id,cast(score_date as string),model_version)) as sid_kw,
 	corp_id ,
 	corp_nm ,
 	credit_cd ,
