b6dd0d3bc917c941809b4aff41957acc249850ad
unknown <huajk@chinacscs.com>
2022-11-25 14:46:10 +0800

    2022-11-25 预警-归因报告 合并段落，修复where筛选语句score_dt书写错误的问题

--- 上线脚本/预警分系(未上线)/归因详报/预警分-归因报告/rmp_warning_score_report.sql
+++ 上线脚本/预警分系(未上线)/归因详报/预警分-归因报告/rmp_warning_score_report.sql
@@ -26,5 +26,5 @@ left join pth_rmp.rmp_warning_score_report3 c
 	on a.batch_dt=c.batch_dt and a.corp_id=c.corp_id and a.score_dt=c.score_dt
 left join pth_rmp.rmp_warning_score_report4 d
 	on a.batch_dt=d.batch_dt and a.corp_id=d.corp_id and a.score_dt=d.score_dt
-where a.score_date=to_date(date_add(from_unixtime(unix_timestamp(cast(${ETL_DATE} as string),'yyyyMMdd')),0))
+where a.score_dt=to_date(date_add(from_unixtime(unix_timestamp(cast(${ETL_DATE} as string),'yyyyMMdd')),0))
 ;
