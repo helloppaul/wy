d29dbaeed16b683872cd6fa2dd2a7709e4451a24
unknown <huajk@chinacscs.com>
2022-12-14 13:54:06 +0800

    2022-12-14 综合舆情分 修复 （1）sid_kw的问题，to_date(batch_dt) -> batch_dt （2）将第一张with临时表的 错误的筛选where逻辑除去

--- 上线脚本/综合舆情分(ok)/rmp_alert_comprehs_score.sql
+++ 上线脚本/综合舆情分(ok)/rmp_alert_comprehs_score.sql
@@ -1,9 +1,4 @@
 -- 综合舆情分 RMP_ALERT_COMPREHS_SCORE (同步方式：一天多批次插入)--
--- with RMP_ALERT_COMPREHS_SCORE_TEMP_ as 
--- (
--- 	select * from 
--- )
-
 set hive.exec.parallel=true;
 set hive.auto.convert.join=ture; 
 
@@ -28,7 +23,6 @@ RMP_ALERT_COMPREHS_SCORE_TEMP_BATCH as
 	from pth_rmp.RMP_ALERT_COMPREHS_SCORE_TEMP a
 	 join (select max(batch_dt) as max_batch_dt,score_dt from pth_rmp.RMP_ALERT_COMPREHS_SCORE_TEMP  group by score_dt) b
 		on a.batch_dt=b.max_batch_dt and a.score_dt=b.score_dt 
-	where a.score_dt=to_date(date_add(from_unixtime(unix_timestamp(cast(${ETL_DATE} as string),'yyyyMMdd')),0))
 ),
 --—————————————————————————————————————————————————————— 应用层 ————————————————————————————————————————————————————————————————————————————————--
 RMP_ALERT_COMPREHS_SCORE_TEMP_RESULT as
@@ -65,36 +59,8 @@ RMP_ALERT_COMPREHS_SCORE_TEMP_RESULT as
 )
 insert into pth_rmp.RMP_ALERT_COMPREHS_SCORE partition(etl_date=${ETL_DATE})
 select 
-	md5(concat(to_date(a.batch_dt),nvl(a.corp_id,''),'0')) as sid_kw,
+	md5(concat(a.batch_dt,nvl(a.corp_id,''),'0')) as sid_kw,
 	a.*
 from RMP_ALERT_COMPREHS_SCORE_TEMP_RESULT a
--- where a.score_dt=to_date(date_add(from_unixtime(unix_timestamp(cast(${ETL_DATE} as string),'yyyyMMdd')),0))
+where a.score_dt=to_date(date_add(from_unixtime(unix_timestamp(cast(${ETL_DATE} as string),'yyyyMMdd')),0))
 ;
--- select distinct
--- 	--a.sid_kw,modify yangcan 20221113 sid_kw是由corp_id,relation_id拼接后转换而来,此处distinct去重没有效果
--- 	md5(concat(to_date(a.batch_dt),nvl(a.corp_id,''),'0')) as sid_kw,
--- 	a.batch_dt,
--- 	a.corp_id,
--- 	a.corp_nm,
--- 	a.credit_code,
--- 	a.score_dt,
--- 	a.comprehensive_score,
--- 	a.score_hit,
--- 	a.label_hit,
--- 	a.alert,
--- 	a.fluctuated,
--- 	a.model_version,
--- 	a.adjust_warnlevel,
--- 	0 as delete_flag,
--- 	'' as create_by,
--- 	current_timestamp() as create_time,
--- 	'' as update_by,
--- 	current_timestamp() update_time,
--- 	0 as version
--- from pth_rmp.RMP_ALERT_COMPREHS_SCORE_TEMP a
--- join (select max(batch_dt) as max_batch_dt,score_dt from pth_rmp.RMP_ALERT_COMPREHS_SCORE_TEMP group by score_dt) b
--- 	on a.batch_dt=b.max_batch_dt and a.score_dt=b.score_dt
-
-
-;
-
